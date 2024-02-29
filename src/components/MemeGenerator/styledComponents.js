3c8a8d2c2356677de7603f961ed54add958d7362
bankavinay <137375311+bankavinay@users.noreply.github.com>
2024-02-29 12:22:45 +0530

    Update styledComponents.js
    new

--- src/components/MemeGenerator/styledComponents.js
+++ src/components/MemeGenerator/styledComponents.js
@@ -2,8 +2,8 @@ import styled from 'styled-components/macro'
 
 export const AppContainer = styled.div`
     display: flex;
-    justfy-content: center;
-    align-item: center;
+    justify-content: center;
+    align-items: center;
     min-height: 100vh;
 `
 export const MemeGeneratorContainer = styled.div`
@@ -11,15 +11,15 @@ export const MemeGeneratorContainer = styled.div`
     flex-direction: column,
     width: 100%;
     max-width: 550px;
-    padding: 32px 32px 24px 24px;
-    @media screen and (min-width: 768px){
-        width: 85%;
-        max-width: 1140px;
-    }
+    padding-top: 32px;
+    padding-bottom: 32px;
+    padding-right: 24px;
+    padding-left: 24px;
+   
 `
 export const Heading = styled.h1`
-    color: "#35469c",
-    font-family: "Open Sans";
+    color: #35469c;
+    font-family: 'Open Sans';
     font-size: 24px;
     font-weight: bold;
     text-align: center;
@@ -42,7 +42,7 @@ export const MemeGeneratorForm = styled.form`
 `
 export const CustomLable = styled.lable`
     color:  #7e858e;
-    font=family: "Open sans";
+    font=family: 'Open Sans';
     font-wight: 500;
     font-size: 10px;
     line-height: 1.4;
@@ -52,29 +52,26 @@ export const CustomLable = styled.lable`
 export const CustomInput = styled.input`
     color: #5a7184;
     baground-color: #ffffff;
-    font-family: "Open sans";
+    font-family: 'Open Sans';
     height: 40px;
     border: 1px solid #d7dfe9;
     border-radies: 5px;
     outline: none;
-    padding: 12px 12px 14px 14px;
+    
 
 `
 export const CustomSelect = styled.select`
     color: #1e293b;
     background-color: #ffffff;
-    font-family: "Open sans";
+    font-family: 'Open Sans';
     height: 40px;
     border: 1px solid #d7dfe9;
     border-radies: 5px;
     outline: none;
-    padding: 12px 12px 14px 14px;
+    
 `
 export const CustomOption = styled.option`
-    font-size: 16px;
-    padding: 12px 12px 14px 14px;
-
-`
+    font-size: 16px;`
 export const GenerateButton = styled.button`
     color: #ffffff;
     baground-color: #0b69ff;
@@ -83,6 +80,23 @@ export const GenerateButton = styled.button`
     max-width: 15px;
     cursor: pointer:
     outline: none;
-    padding: 12px 12px 24px 24px;
+    
     margin-top: 15px;
 `
+export const MemeContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    text-align: center;
+    background-image: url(${props => props.backgroundImage});
+    background-size: cover;
+    height: 300px;
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-bottom: 50px;`
+export const TextContent = styled.p`
+    color: #ffffff;
+    font-size: ${props => props.activeFontSizeId};
+    font-weight: 600;
+    font-family: 'Open Sans';`
